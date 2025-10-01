c4b5a3ecf5e44b662129e503185a71e250a40364
Simran Rai <simranrai192@gmail.com>
2025-10-01 23:54:29 +0530

    Intial Commit

---
+++ l_and_s_element.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largest(vector<int>& nums) {
+        int maximum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > maximum) {
+                maximum = nums[i];
+            }
+        }
+        return maximum;
+    }
+
+    int smallest(vector<int>& nums) {
+        int minimum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < minimum) {
+                minimum = nums[i];
+            }
+        }
+        return minimum;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {5, 8, 3, 9};
+
+    cout << "Largest number = " << sol.largest(nums) << endl;
+    cout << "Smallest number = " << sol.smallest(nums) << endl;
+
+    return 0;
+}
